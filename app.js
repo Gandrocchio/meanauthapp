adee6c68ab8ef0fcf8866d806e00111dfe74ffd7
Gandrocchio <ivellius@hotmail.it>
2017-08-31 07:53:26 -0700

    add prodotto

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ mongoose.connection.on('error', () => {
 const app = express();
 
 const users = require('./routes/users');
+const prodotti = require('./routes/prodotti')
 
 // Port Number
 const port = 3000;
@@ -42,6 +43,7 @@ app.use(passport.session());
 require('./config/passport')(passport);
 
 app.use('/users', users);
+app.use('/prodotti', prodotti);
 
 // Index Route
 app.get('/', (req, res) => {
